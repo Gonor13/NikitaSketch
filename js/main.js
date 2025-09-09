7e4d13373d7de7c4a483d2baafd1d6e51492f6d1
Gonor13 <36681673+Gonor13@users.noreply.github.com>
2025-09-09 23:17:26 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('year').textContent = new Date().getFullYear();
-  const btn = document.getElementById('cta');
-  btn.addEventListener('click', e => {
-    e.preventDefault();
-    alert("Кнопка работает!");
-  });
-});
